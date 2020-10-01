4df51026973c1d3cc91f467c835febb5c33d4eba
Yago Cunha <yago.cunha123@gmail.com>
2020-10-01 16:00:56 -0300

    Creating RedefinitionSentSuccess screen

---
+++ src/pages/RedefinitionSentSuccess/index.tsx
@@ -0,0 +1,40 @@
+import {
+  Container,
+  GoToLoginButtom,
+  GoToLoginButtomText,
+  ImageBackground,
+  SuccessImage,
+  SuccessSubtitle,
+  SuccessTitle,
+} from './styles';
+
+import React, { useCallback } from 'react';
+import backgroundImg from '../../assets/images/signup-success-background.png';
+import successImg from '../../assets/images/signup-sucess.png';
+import { useNavigation } from '@react-navigation/native';
+
+const RedefinitionSentSuccess: React.FC = () => {
+  const { goBack } = useNavigation();
+
+  const handleFinished = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <ImageBackground resizeMode="contain" source={backgroundImg}>
+        <SuccessImage source={successImg} />
+        <SuccessTitle>Redefinição enviada!</SuccessTitle>
+        <SuccessSubtitle>
+          Boa, agora é só checar o e-mail que foi enviado para você redefinir
+          sua senha e aproveitar os estudos.
+        </SuccessSubtitle>
+      </ImageBackground>
+      <GoToLoginButtom onPress={handleFinished}>
+        <GoToLoginButtomText>Voltar ao login</GoToLoginButtomText>
+      </GoToLoginButtom>
+    </Container>
+  );
+};
+
+export default RedefinitionSentSuccess;
